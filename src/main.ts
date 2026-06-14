60e10be46816d31d8efa1002414219696b1244ae
Giss <panaponsukcharoen@email.com>
2026-06-14 10:33:29 +0700

    fix: enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3001);
   console.log(
     `Notification service is running on port ${process.env.PORT ?? 3001}`,
